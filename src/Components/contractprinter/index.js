60f71e151ba27700e19190841dec668601daa4b4
lorenz.moser <mslorenz11@gmail.com>
2024-04-27 20:02:52 +0200

    5ter Commit - added components for later use, some comments to explain functionalities, removed some old code from the initial project

---
+++ src/Components/contractprinter/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+import { useState } from "react";
+
+//function to print a contract based on userinputs
+export default function ContractPrinter(){
+
+};
---
+++ src/Components/pricecalculator/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+import { useState } from "react";
+
+//Function to calculate the approx price of an agile fixpreis project
+export default function PriceCalculator(){
+    
+};
